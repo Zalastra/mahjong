36c4e2321d63919c3246ee24fd2f5ab7f8f23bf8
Maarten Bonsema <maarten.bonsema@gmail.com>
2018-09-01 19:41:09 +0200

    move shuffle algorythm in its own folder

--- src/board/tiles/mod.rs
+++ src/board/tiles/mod.rs
@@ -1,8 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap};
 use std::path::{Path, PathBuf};
 use std::cmp::Ordering::*;
 
-use rand::{thread_rng, Rng};
 use sdl2::render::{Renderer, Texture};
 use sdl2::image::LoadTexture;
 
@@ -10,12 +9,15 @@ use sdl;
 
 mod models;
 mod types;
+mod shuffle;
+mod position;
 
 use self::models::Models;
 use self::types::TileType;
+use self::shuffle::shuffle;
+use self::position::{Position, Neighbour, Direction};
 
 use self::Direction::*;
-use self::CreationState::*;
 use self::PlayState::*;
 
 static ERROR_MESSAGE: &'static str = "error loading texture";
@@ -47,7 +49,11 @@ impl Tiles {
             .collect::<Vec<_>>();
 
         let neighbours = create_neighbour_list(&positions);
-        let types = TileShuffler::new(&positions, &neighbours).shuffle();
+        let mut types = vec![Default::default(); 144];
+
+        shuffle(&mut types, &positions, &neighbours);
+
+        //let types = TileShuffler::new(&positions, &neighbours).shuffle();
         let states = vec![Default::default(); 144];
         let models = Models::new(raw_positions);
         let textures = create_textures();
@@ -69,7 +75,8 @@ impl Tiles {
     }
 
     pub fn reset(&mut self) {
-        self.types = TileShuffler::new(&self.positions, &self.neighbours).shuffle();
+        shuffle(&mut self.types, &self.positions, &self.neighbours);
+
         for tile in 0..144 {
             self.states[tile] = Blocked;
         }
@@ -173,86 +180,6 @@ impl Tiles {
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub struct TileId(usize);
 
-#[derive(Clone, Copy, Debug, Default, PartialEq)]
-struct Position {
-    x: u8,
-    y: u8,
-    z: u8,
-}
-
-impl Position {
-    fn neighbours(&self, other: Position) -> Option<Direction> {
-        if self.is_up_neighbour(other) { Some(Up) }
-        else if self.is_down_neighbour(other) { Some(Down) }
-        else if self.is_left_neighbour(other) { Some(Left) }
-        else if self.is_right_neighbour(other) { Some(Right) }
-        else { None }
-    }
-
-    fn is_up_neighbour(&self, other: Position) -> bool {
-        self.z + 1 == other.z && self.is_potential_vertical_neighbour(other)
-    }
-
-    fn is_down_neighbour(&self, other: Position) -> bool {
-        self.z == other.z + 1 && self.is_potential_vertical_neighbour(other)
-    }
-
-    fn is_potential_vertical_neighbour(&self, other: Position) -> bool {
-        self.x <= other.x + 1 && self.x + 1 >= other.x &&
-        self.y <= other.y + 1 && self.y + 1 >= other.y
-    }
-
-    fn is_left_neighbour(&self, other: Position) -> bool {
-        self.x == other.x + 2 && self.is_potential_horizontal_neighbour(other)
-    }
-
-    fn is_right_neighbour(&self, other: Position) -> bool {
-        self.x + 2 == other.x && self.is_potential_horizontal_neighbour(other)
-    }
-
-    fn is_potential_horizontal_neighbour(&self, other: Position) -> bool {
-        self.z == other.z &&
-        self.y <= other.y + 1 && self.y + 1 >= other.y
-    }
-}
-
-impl<'a> From<&'a (u8, u8, u8)> for Position {
-    fn from(&(x, y, z): &(u8, u8, u8)) -> Self {
-        Self { x, y, z }
-    }
-}
-
-#[derive(Clone, Copy, Debug, PartialEq)]
-enum Direction {
-    Up,
-    Down,
-    Right,
-    Left,
-}
-
-impl Direction {
-    fn rev(self) -> Direction {
-        match self {
-            Left => Right,
-            Right => Left,
-            Up => Down,
-            Down => Up,
-        }
-    }
-}
-
-#[derive(Clone, Copy, Debug)]
-struct Neighbour {
-    id: usize,
-    direction: Direction,
-}
-
-impl Neighbour {
-    fn new(id: usize, direction: Direction) -> Self {
-        Self { id, direction }
-    }
-}
-
 fn create_neighbour_list(positions: &[Position]) -> Vec<Vec<Neighbour>> {
     let mut neighbour_list = vec![Vec::new(); 144];
 
@@ -313,289 +240,9 @@ fn create_textures() -> HashMap<TextureId, Texture> {
     textures
 }
 
-fn get_tile_types() -> Vec<TileType> {
-    let mut tile_types = Vec::new();
-    for tile_type in TileType::iter() {
-        for _ in 0..tile_type.max_allowed() {
-            tile_types.push(*tile_type);
-        }
-    }
-    tile_types
-}
-
 #[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
 enum TextureId {
     Face(TileType, bool),
     Bottom,
     Side,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq)]
-enum CreationState {
-    Unplaced,
-    Placable,
-    Placed,
-}
-
-impl Default for CreationState {
-    fn default() -> Self {
-        Unplaced
-    }
-}
-
-struct TileShuffler<'a> {
-    positions: &'a [Position],
-    neighbours: &'a [Vec<Neighbour>],
-    states: Vec<CreationState>,
-}
-
-impl<'a> TileShuffler<'a> {
-    fn new(positions: &'a [Position], neighbours: &'a [Vec<Neighbour>]) -> Self {
-        Self { positions, neighbours, states: vec![Default::default(); 144] }
-    }
-
-    fn shuffle(mut self) -> Vec<TileType> {
-        let mut types = vec![Default::default(); 144];
-        loop {
-            self.set_random_starting_creation_tiles();
-            
-            if let Ok(_) = self.try_shuffle(&mut types) {
-                break;
-            } else {
-                self.states = vec![Default::default(); 144]
-            }
-        }
-        types
-    }
-
-    fn set_random_starting_creation_tiles(&mut self) {
-        for mut group in self.get_grouped_ground_tiles() {
-            while !group.is_empty() {
-                let random_index = thread_rng().gen_range(0, group.len());
-                let tile = group[random_index];
-
-                self.states[tile] = Placable;
-
-                group = group.into_iter().filter(|&tile| {
-                    self.states[tile] == Unplaced && self.no_placable_neighbour_in_row(tile)
-                }).collect()
-            }
-        }
-    }
-
-    fn no_placable_neighbour_in_row(&self, tile: usize) -> bool {
-        self.no_placable_neighbour_in_row_direction(tile, Left) &&
-        self.no_placable_neighbour_in_row_direction(tile, Right)
-    }
-
-    fn no_placable_neighbour_in_row_direction(&self, tile: usize, direction: Direction) -> bool {
-        let mut neighbours = self.neighbours[tile].clone();
-        while let Some(neighbour) = neighbours.pop() {
-            if neighbour.direction == direction {
-                if self.states[neighbour.id] == Placable {
-                    return false;
-                }
-                neighbours.extend_from_slice(&self.neighbours[neighbour.id]);
-            }
-        }
-        true
-    }
-
-    fn get_grouped_ground_tiles(&self) -> Vec<Vec<usize>> {
-        let mut position_groups = Vec::new();
-        let mut visited = HashSet::new();
-
-        for tile in 0..144 {
-            if visited.contains(&tile) { continue }
-
-            let mut group = Vec::new();
-            let mut neighbours = self.neighbours[tile].clone();
-
-            while let Some(neighbour) = neighbours.pop() {
-                if visited.contains(&neighbour.id) { continue }
-                visited.insert(neighbour.id);
-
-                if self.positions[neighbour.id].z == 0 {
-                    group.push(neighbour.id);
-                    neighbours.extend_from_slice(&self.neighbours[neighbour.id]);
-                }
-            }
-
-            position_groups.push(group);
-        }
-
-        position_groups
-    }
-
-    fn try_shuffle(&mut self, types: &mut [TileType]) -> Result<(), ()> {
-        let mut available_types = get_tile_types();
-        let mut tiles_placed = 0;
-
-        loop {
-            if cfg!(feature = "debug") {
-                // TODO: figure out a way to be able to render the board during the shuffle
-                //self.debug_render();
-                //sdl::wait_for_click();
-            }
-
-            /*
-            TODO:   Figure out an actual working strategy to prevent the creation of an unfinished board.
-            
-            IDEA:   We would need to look up the number of tiles that must be placed for an unplaced tile
-                    and see if that number matches the amount of turns of placing tiles left. This could
-                    still lead to problems when there's multiple of these tiles that depend on other tiles
-                    being placed.
-
-            Code below in placable_tiles_adjusted is a slight improvement reducing the number of failed boards
-            */ 
-
-            let mut tiles = self.placable_tiles_adjusted(tiles_placed);
-
-            if tiles.len() < 2 {
-                return Err(());
-            }
-
-            let random_index = thread_rng().gen_range(0, available_types.len() / 2) * 2;
-            let tile_type1 = available_types.swap_remove(random_index + 1);
-            let tile_type2 = available_types.swap_remove(random_index);
-
-            let random_index = thread_rng().gen_range(0, tiles.len());
-            let tile_id1 = tiles.swap_remove(random_index);
-
-            let random_index = thread_rng().gen_range(0, tiles.len());
-            let tile_id2 = tiles.swap_remove(random_index);
-
-            self.set_placed(tile_id1);
-            self.set_placed(tile_id2);
-
-            types[tile_id1] = tile_type1;
-            types[tile_id2] = tile_type2;
-
-            tiles_placed += 2;
-
-            if tiles_placed == self.positions.len() {
-                return Ok(());
-            }
-        }
-    }
-
-    fn set_placed(&mut self, tile: usize) {
-        self.states[tile] = Placed;
-        self.update_neighbour_creation_states(tile);
-    }
-
-    fn update_neighbour_creation_states(&mut self, tile: usize) {
-        for neighbour in &self.neighbours[tile] {
-            /*
-            all down neighbours must be placed
-            
-            if updated by side neighbour:
-                same y: always place
-                different y: all neighbours on reverse direction must be placed
-            if update from bottom neighbour:
-                if entire row Unplaced: place
-                if side neighbour with same y is placed: place
-                |TODO| if direct neighbour is Placable, rest of row is Unplaced: place
-            if update from top neighbour:
-                tile should already be placed
-            */
-            match (self.states[neighbour.id], neighbour.direction) {
-                (Unplaced, direction @ Left) | (Unplaced, direction @ Right) => {
-                    let all_down = self.all_neighbours_in_direction_placed(neighbour.id, Down);
-                    let same_y = self.positions[tile].y == self.positions[neighbour.id].y;
-                    let all_placed_in_source_direction =
-                        self.all_neighbours_in_direction_placed(neighbour.id, direction.rev());
-                    
-                    if all_down && (same_y || all_placed_in_source_direction) {
-                        self.states[neighbour.id] = Placable;
-                    }
-                },
-                (Unplaced, Up) => {
-                    let all_down = self.all_neighbours_in_direction_placed(neighbour.id, Down);
-                    let unplaced = self.row_unplaced(neighbour.id);
-                    let same_y_placed = self.same_y_neighbour_placed(neighbour.id);
-
-                    if all_down && (unplaced || same_y_placed) {
-                        self.states[neighbour.id] = Placable;
-                    }
-                },
-                (_, _) => (),
-            }
-        }
-    }
-
-    fn row_unplaced(&self, tile: usize) -> bool {
-        self.all_recursive_neighbours_unplaced(tile, Left) &&
-        self.all_recursive_neighbours_unplaced(tile, Right)
-    }
-
-    fn same_y_neighbour_placed(&self, tile: usize) -> bool {
-        self.neighbours[tile].iter().any(|neighbour| {
-            let direction = neighbour.direction;
-            let same_y = self.positions[tile].y == self.positions[neighbour.id].y;
-            let placed = self.states[neighbour.id] == Placed;
-
-            (direction == Left || direction == Right) && same_y && placed
-        })
-    }
-
-    fn all_neighbours_in_direction_placed(&self, tile: usize, direction: Direction) -> bool {
-        self.neighbours[tile]
-            .iter()
-            .filter(|neighbour| neighbour.direction == direction)
-            .all(|neighbour| self.states[neighbour.id] == Placed)
-    }
-
-    fn all_recursive_neighbours_unplaced(&self, tile: usize, direction: Direction) -> bool {
-        let mut neighbours = self.neighbours[tile].clone();
-
-        while let Some(neighbour) = neighbours.pop() {
-            if neighbour.direction == direction {
-                if self.states[neighbour.id] != Unplaced {
-                    return false;
-                }
-                neighbours.extend_from_slice(&self.neighbours[neighbour.id]);
-            }
-        }
-        true
-    }
-
-    fn placable_tiles_adjusted(&self, amount_placed: usize) -> Vec<usize> {
-        let mut tiles = self.placable_tiles();
-
-        match (amount_placed, tiles.len()) {
-            (142, 2) => (),
-            (_, 3) => {
-                let mut new_tiles = Vec::new();
-                let mut ignored = false;
-                for &tile in &tiles {
-                    let mut ignore = !ignored;
-                    for neighbour in &self.neighbours[tile] {
-                        if self.states[neighbour.id] != Placed {
-                            ignore = false;
-                            break;
-                        }
-                    }
-                    if ignore {
-                        ignored = true;
-                    } else {
-                        new_tiles.push(tile);
-                    }
-                }
-                tiles = new_tiles;
-            },
-            _ => (),
-        }
-
-        tiles
-    }
-
-    fn placable_tiles(&self) -> Vec<usize> {
-        self.states
-            .iter()
-            .enumerate()
-            .filter(|&(_, state)| *state == Placable)
-            .map(|(idx, _)| idx)
-            .collect()
-    }
 }
---
+++ src/board/tiles/position.rs
@@ -0,0 +1,81 @@
+use self::Direction::*;
+
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
+pub struct Position {
+    pub x: u8,
+    pub y: u8,
+    pub z: u8,
+}
+
+impl Position {
+    pub fn neighbours(&self, other: Position) -> Option<Direction> {
+        if self.is_up_neighbour(other) { Some(Up) }
+        else if self.is_down_neighbour(other) { Some(Down) }
+        else if self.is_left_neighbour(other) { Some(Left) }
+        else if self.is_right_neighbour(other) { Some(Right) }
+        else { None }
+    }
+
+    fn is_up_neighbour(&self, other: Position) -> bool {
+        self.z + 1 == other.z && self.is_potential_vertical_neighbour(other)
+    }
+
+    fn is_down_neighbour(&self, other: Position) -> bool {
+        self.z == other.z + 1 && self.is_potential_vertical_neighbour(other)
+    }
+
+    fn is_potential_vertical_neighbour(&self, other: Position) -> bool {
+        self.x <= other.x + 1 && self.x + 1 >= other.x &&
+        self.y <= other.y + 1 && self.y + 1 >= other.y
+    }
+
+    fn is_left_neighbour(&self, other: Position) -> bool {
+        self.x == other.x + 2 && self.is_potential_horizontal_neighbour(other)
+    }
+
+    fn is_right_neighbour(&self, other: Position) -> bool {
+        self.x + 2 == other.x && self.is_potential_horizontal_neighbour(other)
+    }
+
+    fn is_potential_horizontal_neighbour(&self, other: Position) -> bool {
+        self.z == other.z &&
+        self.y <= other.y + 1 && self.y + 1 >= other.y
+    }
+}
+
+impl<'a> From<&'a (u8, u8, u8)> for Position {
+    fn from(&(x, y, z): &(u8, u8, u8)) -> Self {
+        Self { x, y, z }
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl Direction {
+    pub fn rev(self) -> Direction {
+        match self {
+            Left => Right,
+            Right => Left,
+            Up => Down,
+            Down => Up,
+        }
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Neighbour {
+    pub id: usize,
+    pub direction: Direction,
+}
+
+impl Neighbour {
+    pub fn new(id: usize, direction: Direction) -> Self {
+        Self { id, direction }
+    }
+}
